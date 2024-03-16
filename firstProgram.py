40e0b076267b7c5974ed8f7f883b6507caf270ba
DivyachoudhuryNITRR <divyachoudhury930@gmail.com>
2024-03-16 15:02:25 +0530

    add features

---
+++ firstProgram.py
@@ -0,0 +1,139 @@
+# print('Hello World!!')
+
+# print("Divya is my name.","My age is 21.")
+# print(37)
+# print(37+107)
+
+# name="Divya" #string
+# age=21 #integer
+# price=50.99 #float
+# age2=age
+# print("my name is ", name)
+# print("my age is ", age)
+# print("price is ", price)
+
+# print(age2)
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+# a=None
+# print(type(a))
+
+
+# a=37
+# b=24
+# sum=a+b
+# diff=a-b
+# print(sum)
+# print(diff)
+
+"""print("Hello")
+    how are you"""
+
+
+# ARITHMETIC Operators
+# a=5
+# b=2
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b) #floating values we get
+
+# #MODULO OPERATORS
+# a=6
+# b=2
+# print(a%b)
+
+# #power operators
+# a=2
+# b=3
+# print(a**b)
+
+# # Relational operator
+# a=50
+# b=20
+# c=50
+
+# print(a==b)
+# print(a==c)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+
+# ASSIGNMENT OPERATOR
+# num=10
+# # num=num+10
+# # num+=10 #assignment operator
+# # num-=10
+# # num*=10
+# # num/=10
+# num%=10
+# num**=2
+# print(num)
+
+
+# LOGICAL OPERATORS
+# not
+# a=50
+# b=30
+
+# print(not False)
+# print(not (a>b))
+
+# val1=True
+# val2=False
+# print("and operator : ", val1 and val2)
+# print("or operator : ", val1 or val2)
+# print((a==b) or (a>b))
+
+
+# a=2
+# b=4.25
+# sum=a+b
+# print(sum)
+
+
+# #TYPE CASTING
+# a=int("2")
+# b=4.25
+# print(a+b)
+
+# name=input("enter your name : ")
+# print("Welcome",name)
+
+# val=input("enter some values : ")
+# print(type(val),val) #default type == string
+
+
+# val=int(input("enter some values : "))
+# print(type(val),val)
+
+#***********SUM OF 2 NUMBERS***********
+# a=int(input("enter a numbers : "))
+# b=int(input("enter  a numbers : "))
+# sum=a+b
+# print(sum)
+
+
+#**********AREA OF SQUARE****************
+# a=int(input("enter side of square : "))
+# area=a**2
+# print(area)
+
+#***************Avrage***********
+# a=float(input("enter a : "))
+# b=float(input("enter b : "))
+# avg=(a+b)/2
+# print(avg)
+
+#******Compare*********
+# a=float(input("enter a number : "))
+# b=float(input("enter the other number : "))
+# if a>=b :
+#     print("True")
+# else:
+#     print("False")
