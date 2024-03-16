49d9cd01c21f5ab1641c065efa38eaada3191769
DivyachoudhuryNITRR <divyachoudhury930@gmail.com>
2024-03-16 23:18:13 +0530

    add new features

---
+++ lecture2.py
@@ -0,0 +1,102 @@
+# str1="This is a string."
+# str2='Apna College'
+# str3="""This is a string"""
+
+# "This is apnaCollege's lecture" --> for this we apply single and double quotes
+
+# str1="This is a string,
+# we are here to learn" -->invalid way of giving the string
+
+# Escape Squence characters
+# str1="This is a string.\t we are creating projects"
+# print(str1)
+# str2="This is a string.\n we are creating projects"
+# print(str2)
+
+# CONCATENATE
+# str1="apna"
+# str2="college"
+# final=str1+" "+str2
+# # print(final)
+
+# #length
+# print(len(str1))
+# print(len(str2))
+# print(len(final))
+
+
+# str="apna college"
+# # ch=str[1]
+# # str[4]='G'
+# # print(str)
+# print(str[0:4])
+
+
+# str="i am studying python from apnacollege"
+# print(str.endswith("ge"))
+# print(str.capitalize())
+# str=str.capitalize()
+# print(str)
+
+# print(str.replace("o","a"))
+# print(str.find("o"))
+# print(str.find("z"))
+# # print(str.count("o"))
+
+# name=input("enter the first name: ")
+# print(len(name))
+
+# str1="Thebvjhasd $$ nkjdsd $ jasdhjhs"
+# print(str1.count("$"))
+
+# num=5
+# if(num>2):
+#     print("greater than 2")
+# elif(num>3):
+#     print("greater than 3")
+# print("You are eligible")
+
+
+# marks=float(input("Enter the marks : "))
+# if(marks>=90):
+#     print("A")
+# elif(marks>=80):
+#     print("B")
+# elif(marks>=70):
+#     print("C")
+# else:
+#     print("D")
+
+
+# nesting
+# age=95
+# if(age>=18):
+#     if(age>=80):
+#         print("cannot drive")
+#     else:
+#         print("Can drive")
+# else:
+#     print("cananot drive") 
+
+# num=int(input("enter a number : "))
+# if(num%2==0):
+#     print("even")
+# else:
+#     print("odd")
+
+# a=int(input("enter a number = "))
+# b=int(input("enter a number = "))
+# c=int(input("enter a number = "))
+# if(a>b and a>c):
+#     print(a," is greatest.")
+# elif(b>a and b>c):
+#     print(b," is greatest.")
+# else:
+#     print(c," is greatest.")
+
+
+# num=int(input("enter a number : "))
+# if(num%7==0):
+#     print("multiple of 7")
+# else:
+#     print("not a multiple of 7")
