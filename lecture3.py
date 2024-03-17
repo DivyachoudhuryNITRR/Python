cdd3d9cb9751ff60188c532e89ba55961e3f1da7
DivyachoudhuryNITRR <divyachoudhury930@gmail.com>
2024-03-17 09:31:33 +0530

    Add features

---
+++ lecture3.py
@@ -0,0 +1,59 @@
+marks=[94.4,87.5,95.2,68.4,45.1]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(len(marks))
+# marks[0]="Divya"
+# print(marks)
+
+# print(marks[2:4])
+# print(marks[2:])
+# print(marks[:5])
+# print(marks[-3:-1])
+
+
+#TUPLE
+# tup=(2,1,3,1)
+# print(type(tup))
+# print(tup[0])
+# # tup[0]=3
+# t=()
+# print(t)
+# print(type(t))
+# tt=(1)
+# print(type(tt))
+# t1=('Hello')
+# print(type(t1))
+
+
+
+# tup=(2,1,3,1)
+# print(tup.index(1))
+# print(tup.count(1))
+
+
+# movies=[]
+# mov1=input("enter mov1 : ")
+# mov2=input("enter mov1 : ")
+# mov3=input("enter mov1 : ")
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+# print(movies)
+# print(type(movies))
+
+# list=[1,2,9,1]
+# c=list.copy()
+# c.reverse()
+# if(list==c):
+#     print("yes")
+# else:
+#     print("no")
+
+# list=["C","D","A","A","B","B","A"]
+# # print(list.count("A"))
+# list.sort()
+# print(list)
+
