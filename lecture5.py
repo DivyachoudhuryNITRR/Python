11784540e347206dbb546f3287e91f03e377b6dd
DivyachoudhuryNITRR <divyachoudhury930@gmail.com>
2024-03-18 23:00:15 +0530

    add feature

---
+++ lecture5.py
@@ -0,0 +1,136 @@
+#LOOPS
+
+# i=1
+# while i<=5:
+#     # print("Hello")
+#     print(i)
+#     i=i+1
+# print("loop ended")
+
+
+# i=1
+# while i<=100:
+#     print(i)
+#     i=i+1
+# print("loop ended")
+
+# j=100
+# while j>=1:
+#     print(j)
+#     j=j-1
+# print("Game ended")
+
+
+# n=int(input("Enter the number : "))
+# i=1
+# while i<=10:
+#     print(n,"*",i,"=",n*i)
+#     i=i+1
+# print("table printed")
+
+# i=1
+# while i<=10:
+#     print(i**2)
+#     i=i+1
+
+
+# nums=(1,4,9,16,25,36,49,64,81,100)
+# x=36
+# i=0
+# while i< len(nums):
+#     if(nums[i]==x):
+#         print(i)
+#         # continue
+#         break
+#     else:
+#         print("finding...")
+    
+#     i=i+1
+
+
+# i=1
+# while i<=5:
+    
+#     if(i%2!=0):
+#         i=i+1
+#         # break
+#         continue
+#     print(i)
+#     i=i+1
+
+##FOR Loops
+
+
+# list=[1,2,3,4]
+# for i in list :
+#     print(i)
+# else:
+#     print("List ended")
+
+# names=("divya","rahul","dinesh")
+# for i in names:
+#     print(i)
+# print("Are good friends")
+
+# str="Divyachoudhury"
+# for char in str:
+#     if(char =='o'):
+#         print("o found")
+#         break
+#     print(char)
+# else:
+#     print("End")
+
+
+# for i in range(1,11):
+#     print(i*i)
+
+# tuple=(1,4,9,16,25,36,49,64,81,100)
+# n=16
+# for i in tuple:
+#     if(i==n):
+#         print("Found")
+#         break
+#     i=i+1
+
+# Range:
+
+# for i in range(1,11,2):
+#     print(i)
+
+# for i in range(1,101):
+#     print(i)
+
+
+# for i in range(100,0,-1):
+#     print(i)
+
+# n=7
+# for i in range(1,11,1):
+#     print(n,"*",i,"=",n*i)
+
+# for i in range(5):
+#     pass
+
+
+# n=int(input("Enter the Number : "))
+# i=1
+# sum=0
+# while i<=n:
+#     sum=sum+i
+#     i=i+1
+# print(sum)
+
+p=1
+n=int(input("enter a number : "))
+for i in range(1,n+1,1):
+    p=p*i
+    
+print(p)
+
+
+
+    
+
+    
+   
